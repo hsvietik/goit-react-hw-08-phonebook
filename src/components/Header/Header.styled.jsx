8b62121c5821c96502916045b36fc90f6887d920
Hanna Svietik <hsvietik@gmail.com>
2023-07-29 21:33:21 +0100

    favicon added

--- src/components/Header/Header.styled.jsx
+++ src/components/Header/Header.styled.jsx
@@ -9,7 +9,7 @@ export const StyledHeader = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 10px 50px;
+  padding: 10px 20px;
   background-color: ${Colors.headerBackground};
   box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2),
     0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);
